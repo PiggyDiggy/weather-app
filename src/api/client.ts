99345715ca2cec48e3279ccf4b3aed1074ae80f2
PiggyDiggy <dima-gus04@mail.ru>
2023-10-19 23:45:05 +0300

    Removed unnecessary api route

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,5 @@
-import { restoreDateObjectsInArray } from "@/utils";
-import { Location, sortLocationsByRank } from "@/entities/location";
+import { Location } from "@/entities/location";
 
-import { getAllWeather } from "./qweather";
 import { method } from ".";
 
 function getURL(path: string) {
@@ -10,27 +8,13 @@ function getURL(path: string) {
   return `${url.host}/api/${path}`;
 }
 
-type AllWeather = Awaited<ReturnType<typeof getAllWeather>>;
-
-export function getAllWeatherClient({ locationId }: { locationId: string }) {
-  return method<{ error: string } | AllWeather, AllWeather>({
-    path: getURL("weather"),
-    params: { id: locationId },
-    process(response) {
-      if ("error" in response) {
-        throw new Error(response.error);
-      }
-
-      return {
-        dailyForecast: restoreDateObjectsInArray(response.dailyForecast, ["fxDate", "sunrise", "sunset"]),
-        hourlyForecast: restoreDateObjectsInArray(response.hourlyForecast, ["fxTime"]),
-        currentWeather: response.currentWeather,
-      };
-    },
-  });
-}
-
-export function searchLocationsByName({ locationName, fetchOptions }: { locationName: string, fetchOptions?: RequestInit}) {
+export function searchLocationsByName({
+  locationName,
+  fetchOptions,
+}: {
+  locationName: string;
+  fetchOptions?: RequestInit;
+}) {
   return method<{ error: string } | Location[], Location[]>({
     path: getURL("location"),
     params: { name: locationName },
@@ -40,7 +24,7 @@ export function searchLocationsByName({ locationName, fetchOptions }: { location
         throw new Error(response.error);
       }
 
-      return sortLocationsByRank(response);
+      return response;
     },
   });
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,5 @@ export * from "./createSlides";
 export * from "./classname";
 export * from "./formatTemperature";
 export * from "./formatDate";
-export * from "./restoreDateObjects";
 export * from "./formatLocationName";
 export * from "./debounce";
